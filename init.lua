ffda82f9939bd938c8d1b914bf877c180a073d3e
SkuldNorniern <skuldnorniern@gmail.com>
2023-09-22 10:05:45 +0900

    refactor: move few more pre-defined values into config

--- init.lua
+++ init.lua
@@ -2,13 +2,25 @@ local plugin = {}
 local dkjson = require "dkjson"
 
 local API_KEY = "YOUR_DATADOG_API_KEY" -- Replace with your Datadog API key
-
+local METRICS_ENDPOINT = "https://api.us5.datadoghq.com/api/v2/series"
 local hostname = "YOUR_HOSTNAME"
+local metrics_name = "fluere.flow"
+local pkt_metrics_name = "pkts"
+local octets_metrics_name = "octets"
+local src_port_metrics_name = "port.src"
+local dst_port_metrics_name = "prot.dst"
 
 -- Initialization function
 function plugin.init(config)
   API_KEY = config.api_key or error("No API key provided")
+  METRICS_ENDPOINT = config.endpoint or error("No metrics endpoint provided")
+
   hostname = config.hostname or "fluerehost"
+  metrics_name = config.metrics_name or "fluere.flow"
+  pkt_metrics_name = config.pkt_metrics_name or "pkts"
+  octets_metrics_name = config.octets_metrics_name or "octets"
+  src_port_metrics_name = config.src_port_metrics_name or "port.src"
+  dst_port_metrics_name = config.dst_port_metrics_name or "prot.dst"
 end
 
 
@@ -38,11 +50,11 @@ local function create_payload(data, metric_name, data_field)
           }
         },
         tags = {
-          "source_ip:" .. data.source,
-          "dest_ip:" .. data.destination,
-          "protocol:" .. data.prot,
-          "src_port:" .. data.src_port,
-          "dst_port:" .. data.dst_port
+          "SourceIp:" .. data.source,
+          "DestIp:" .. data.destination,
+          "Protocol:" .. data.prot,
+          "SrcPort:" .. data.src_port,
+          "DstPort:" .. data.dst_port
         },
         unit = ""
       }
@@ -52,13 +64,12 @@ end
 
 -- Process data function
 function plugin.process_data(data)
-  local metrics_endpoint = "https://api.us5.datadoghq.com/api/v2/series"
-  local metrics = {"pkt", "octects", "port.src_port", "port.dst_port"}
+  local metrics = {pkt_metrics_name, octets_metrics_name, src_port_metrics_name, dst_port_metrics_name}
   local data_fields = {"d_pkts", "d_octets", "src_port", "dst_port"}
 
   for i, metric in ipairs(metrics) do
-    local payload = create_payload(data, "fluere.net.flow." .. metric, data_fields[i])
-    execute_curl_command(metrics_endpoint, payload)
+    local payload = create_payload(data, metrics_name .. "." .. metric, data_fields[i])
+    execute_curl_command(METRICS_ENDPOINT, payload)
   end
 end
 
